2ac632f6ed8d2e52614a23285c1fc4a619066b47
ebisa <225586468+ebisa22@users.noreply.github.com>
2026-03-15 19:01:29 +0300

    Add all other weather icons that were missing in support.js

--- src/support.js
+++ src/support.js
@@ -1,13 +1,25 @@
 export const weatherIcons = {
   "clear-day":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/clear-day.svg",
-  "partly-cloudy-day":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/partly-cloudy-day.svg",
-  "rain": "https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/rain.svg",
-  "tornado": "https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/tornado.svg",
   "clear-night":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/clear-night.svg",
-  "clear-day-line":"https://bmcdn.nl/assets/weather-icons/v2.0/line/clear-day.svg",
-  "partly-cloudy-day-line":"https://bmcdn.nl/assets/weather-icons/v2.0/line/partly-cloudy-day.svg",
-  "rain-line": "https://bmcdn.nl/assets/weather-icons/v2.0/line/rain.svg",
-  "tornado-line": "https://bmcdn.nl/assets/weather-icons/v2.0/line/tornado.svg",
-  "clear-night-line":"https://bmcdn.nl/assets/weather-icons/v2.0/line/clear-night.svg",
+  "partly-cloudy-day":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/partly-cloudy-day.svg",
+  "partly-cloudy-night":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/partly-cloudy-day.svg",
+
   "cloudy":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/overcast.svg",
+
+  "rain":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/rain.svg",
+  "showers-day":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/rain.svg",
+  "showers-night":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/rain.svg",
+
+  "snow":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/snow.svg",
+  "snow-showers-day":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/snow.svg",
+  "snow-showers-night":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/snow.svg",
+
+  "thunder-rain":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/thunderstorms-rain.svg",
+  "thunder-showers-day":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/thunderstorms-rain.svg",
+  "thunder-showers-night":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/thunderstorms-rain.svg",
+
+  "fog":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/fog.svg",
+  "wind":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/wind.svg",
+
+  "tornado":"https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/tornado.svg"
 };
